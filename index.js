64551952ea0be0e6d0ddc036e01ce248091c5343
dfghujhg <js1thadeu19@hotmail.com>
2024-05-06 10:49:30 +0700

    feat task

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-const factorialRecursive = (n) => {
-  if (n === 0 || n === 1) {
-    return 1;
+function combine(n, k) {
+  const result = [];
+  backtrack(1, []);
+  return result;
+  function backtrack(start, current) {
+    if (current.length === k) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      current.push(i);
+      backtrack(i + 1, current);
+      current.pop();
+    }
   }
-  return n * factorialRecursive(n - 1);
-};
+}
